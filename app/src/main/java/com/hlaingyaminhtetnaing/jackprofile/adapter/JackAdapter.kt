0a23beb190de5da32ec43295e0f3649708e671ed
hlaingyaminhtetnaing <hlaingyaminhtetnaing@gmail.com>
2020-09-30 19:35:27 +0630

    Updated Profile

--- app/src/main/java/com/hlaingyaminhtetnaing/jackprofile/adapter/JackAdapter.kt
+++ app/src/main/java/com/hlaingyaminhtetnaing/jackprofile/adapter/JackAdapter.kt
@@ -8,22 +8,24 @@ import com.hlaingyaminhtetnaing.jackprofile.R
 import com.hlaingyaminhtetnaing.jackprofile.model.Jack
 import kotlinx.android.synthetic.main.item_eachofme.view.*
 
-class JackAdapter(var jackList:ArrayList<Jack>) : RecyclerView.Adapter<JackAdapter.jackViewHolder>(){
+class JackAdapter(var jackList: ArrayList<Jack>) :
+    RecyclerView.Adapter<JackAdapter.jackViewHolder>() {
     class jackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(jack: Jack) {
             itemView.ImgRe.setImageResource(jack.imgJack)
-            itemView.txtRe.text=jack.textName
+            itemView.txtRe.text = jack.textName
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): jackViewHolder {
-        var view= LayoutInflater.from(parent.context).inflate(R.layout.item_eachofme,parent,false)
-        return  jackViewHolder(view)
+        var view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_eachofme, parent, false)
+        return jackViewHolder(view)
     }
 
-    override fun getItemCount(): Int=jackList.size
+    override fun getItemCount(): Int = jackList.size
 
     override fun onBindViewHolder(holder: jackViewHolder, position: Int) {
-       holder.bind(jackList[position])
+        holder.bind(jackList[position])
     }
 }
